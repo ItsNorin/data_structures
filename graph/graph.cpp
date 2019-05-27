8ff4305b4398f47edda53f000abd3b50df1a2079
ItsNorin <31154012+ItsNorin@users.noreply.github.com>
2019-05-26 22:44:56 -0700

    Added Dijkstra pathfinding

--- graph/graph.cpp
+++ graph/graph.cpp
@@ -1,31 +1,40 @@
 #include "graph.h"
+#include <algorithm>
 
 Graph::Node::Node() 
 {}
 
 Graph::Node::Node(const PointT point)
-	: point(point)
+	: point_(point)
 {}
 
+PointT Graph::Node::point() const {
+	return point_;
+}
 
-bool Graph::Node::operator==(const Graph::Node &n) const { return point == n.point; }
-bool Graph::Node::operator!=(const Graph::Node &n) const { return point != n.point; }
+const std::vector<std::pair<Graph::Node*, int>>& Graph::Node::links() const {
+	return links_;
+}
+
+
+bool Graph::Node::operator==(const Graph::Node &n) const { return point_ == n.point_; }
+bool Graph::Node::operator!=(const Graph::Node &n) const { return point_ != n.point_; }
 
 
 void Graph::Node::linkThis(Node *n, int weight) {
-	for (unsigned i = 0; i < links.size(); i++) {
-		if (links[i].first == n) {
-			links[i].second = weight;
+	for (unsigned i = 0; i < links_.size(); i++) {
+		if (links_[i].first == n) {
+			links_[i].second = weight;
 			return;
 		}
 	}
-	links.push_back(std::make_pair(n, weight));
+	links_.push_back(std::make_pair(n, weight));
 }
 
 void Graph::Node::unlinkThis(Node *n) {
-	for (auto it = links.begin(); it < links.end(); it++) 
+	for (auto it = links_.begin(); it < links_.end(); it++) 
 		if ((*it).first == n) {
-			links.erase(it);
+			links_.erase(it);
 			return;
 		}
 }
@@ -53,10 +62,10 @@ bool Graph::insert(const PointT &point) {
 bool Graph::remove(const PointT &point) {
 	Node *toDel = &map_[point];
 	// remove all links to node
-	while (toDel->links.size() > 0) 
-		toDel->links[0].first->unlink(toDel);
+	while (toDel->links_.size() > 0) 
+		toDel->links_[0].first->unlink(toDel);
 		
-	map_.erase(toDel->point);
+	map_.erase(toDel->point_);
 	return false; 
 }
 
@@ -64,6 +73,82 @@ void Graph::link(const PointT &point, const PointT &neighbor, const int weight) 
 	map_[point].link(&map_[neighbor], weight);
 }
 
-void Graph::unlink(const PointT & point, const PointT & neighbor) {
+void Graph::unlink(const PointT &point, const PointT &neighbor) {
 	map_[point].unlink(&map_[neighbor]);
+}
+
+bool Graph::contains(const PointT & point) const {
+	return map_.find(point) != map_.end();
+}
+
+const std::vector<std::pair<Graph::Node*, int>> Graph::linksOf(const PointT &p) const {
+	if (contains(p))
+		return map_.at(p).links_;
+	else
+		return std::vector<std::pair<Graph::Node*, int>>();
+}
+
+
+// TODO: 
+// test pathfinding to ensure it doesnt break when:
+// - there is no valid path from start to goal
+// - start and goal are the same
+// - start and/or goal aren't in graph
+
+std::vector<PointT> Graph::pathfindDijkstra(const PointT &start, const PointT &goal) const {
+	if (contains(start) && contains(goal)) {
+		std::unordered_map<PointT, Node> unexploredNodes = map_;
+		std::unordered_map<PointT, int> distanceFromStart; // given any point, what is its distance from start
+		std::unordered_map<PointT, const PointT*> pathParents; // previous point for each point in the path
+
+		for (auto it = unexploredNodes.begin(); it != unexploredNodes.end(); it++) {
+			distanceFromStart.insert({ (*it).first, INT_MAX }); // make each starting distance as large as possible
+			pathParents.insert({ (*it).first, nullptr }); // insert an invalid parent point for each point in map
+		}
+
+		distanceFromStart[start] = 0; // distance from start -> start is 0
+
+		while (unexploredNodes.size() > 0) {
+			// get point with least distance from start
+			const PointT *current = &(*unexploredNodes.begin()).first;
+			for (auto it = unexploredNodes.begin(); it != unexploredNodes.end(); it++) {
+				if (distanceFromStart[(*it).first] < distanceFromStart[*current]) 
+					current = &(*it).first;
+			}
+
+			// if path was found, put path into a vector to return
+			if (*current == goal) {
+				std::vector<PointT> path;
+				path.push_back(goal);
+
+				// start from goal, then trace its parents back to current node
+				while (path.back() != start)
+					path.push_back(*pathParents[path.back()]);
+
+				// reverse path to go from start to goal
+				std::reverse(path.begin(), path.end());
+
+				return path;
+			}
+			else {
+				const std::vector<std::pair<Node *, int>> &links = linksOf(*current);
+
+				// for each neighbor of the current node,
+				for (auto it = links.begin(); it < links.end(); it++) {
+					// update their distanceFromStart if it is less than what it is currently
+					PointT neighborPoint = (*it).first->point_;
+					int distance = distanceFromStart[*current] + (*it).second;
+
+					if (distance < distanceFromStart[neighborPoint]) {
+						distanceFromStart[neighborPoint] = distance;
+						pathParents[neighborPoint] = &(*map_.find(*current)).first;
+					}
+				}
+				// remove explored node
+				unexploredNodes.erase(*current);
+			}
+		}
+	}
+
+	return std::vector<PointT>();
 }
--- graph/graph.h
+++ graph/graph.h
@@ -8,7 +8,7 @@
 typedef Point2D PointT; 
 
 // graph of any nDimensional space
-// supports Dijkstra and A* pathfinding
+// supports A* pathfinding
 class Graph {
 protected:
 	// individual node in graph, refering to a point in space of type PointT
@@ -24,15 +24,28 @@ public:
 	// true if insertion was successful
 	bool insert(const PointT &point);
 
-	// removes a point from graph
+	// removes a point from graph after unlinking it
 	// false if point wasn't in graph
 	bool remove(const PointT &point);
 
 	// links two points together with a given weight
-	void link(const PointT &point, const PointT &neighbor, const int weight);
+	// if nodes are already linked, simply updates their weight
+	void link(const PointT &point, const PointT &neighbor, const int weight = 1);
 
 	// unlinks two points
 	void unlink(const PointT &point, const PointT &neighbor);
+
+	// true if point is in hashmap
+	bool contains(const PointT &point) const;
+
+	// links outgoing from node
+	// will be empty if node does not exist
+	const std::vector<std::pair<Node *, int>> linksOf(const PointT &p) const;
+
+public:
+	// finds a short path from start to end
+	// returned vector will be empty if no path was found
+	std::vector<PointT> pathfindDijkstra(const PointT &start, const PointT &goal) const;
 };
 
 
@@ -40,27 +53,33 @@ public:
 // individual node in graph, refering to a point in space of type PointT
 // contains links to other nodes in graph, and weight of each link
 class Graph::Node {
+	friend class Graph;
+
 public:
 	Node();
 	// create a node for a point in space
 	Node(const PointT point);
 
+	PointT point() const;
+	const std::vector<std::pair<Node *, int>> & links() const;
+
 	// makes two nodes neighbors
 	// each node will insert the other and the given weight into their links vector
 	// if node is already neighbors, will only update weight
-	void link(Node *n, int weight = 1);
+	void link(Node *n, int weight);
 
 	// unlinks two nodes
 	void unlink(Node *n);
 
 	bool operator==(const Graph::Node &n) const;
 	bool operator!=(const Graph::Node &n) const;
-public:
+
+protected:
 	// point this node represents
-	PointT point;
+	PointT point_;
 
 	// any neighbors this node might have and a weight to get to them
-	std::vector<std::pair<Node *, int>> links;
+	std::vector<std::pair<Node *, int>> links_;
 
 private:
 	// make only this node have given graph node as a neighbor
